4e06195d08b613439b627c14d56bcac00621a92b
Albertino Vieira <8140346@estg.ipp.pt>
2023-09-08 20:04:37 +0100

    fix white line in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border z-10 border-t-[#33353F] border-b-transparent border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span>LOGO</span>
         <p className="text-slate-600">All rights reserved.</p>
