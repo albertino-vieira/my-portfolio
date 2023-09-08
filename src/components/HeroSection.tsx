2c0c800a085952f133c60475392210a2b1560d0b
Albertino Vieira <8140346@estg.ipp.pt>
2023-09-08 19:42:31 +0100

    added cv to download

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -45,8 +45,9 @@ const HeroSection = () => {
               Hire Me
             </a>
             <a
-              href="/"
+              href='../assets/Albertino_Vieira_Resume.pdf'
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              download
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
