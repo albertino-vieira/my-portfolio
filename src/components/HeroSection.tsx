91d4b09f6cb5845151d3910ef999f9dc947087dd
Albertino Vieira <8140346@estg.ipp.pt>
2023-09-08 19:43:34 +0100

    changed dir to public

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
               Hire Me
             </a>
             <a
-              href='../assets/Albertino_Vieira_Resume.pdf'
+              href='../../public/Albertino_Vieira_Resume.pdf'
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
               download
             >
